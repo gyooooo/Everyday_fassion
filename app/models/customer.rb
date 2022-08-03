b1360bb916061262c957a2ebcd547c6a55137ba5
gyooooo <harapeko2925@gmail.com>
2022-08-03 14:41:53 +0000

    add

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,12 +3,6 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  # def self.guest
-  #   find_or_create_by!(email: 'guest@test.com') do |user|
-  #     user.password = SecureRandom.urlsafe_base64 # ランダムなパスワードを生成
-  #   end
-  # end
   
   def self.guest
     find_or_create_by!(email: 'guest@test.com', last_name: 'guest', first_name: 'guest') do |customer|
