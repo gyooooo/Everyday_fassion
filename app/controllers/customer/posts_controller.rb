df343e493e7ae716e6824aef13b69f8bc011693c
gyooooo <harapeko2925@gmail.com>
2022-08-22 12:49:58 +0000

    css

--- app/controllers/customer/posts_controller.rb
+++ app/controllers/customer/posts_controller.rb
@@ -13,7 +13,8 @@ class Customer::PostsController < ApplicationController
       @post.save_tag(tag_list)                                                           
       redirect_to customer_posts_path
     else
-      redirect_to new_customer_post_path
+      #redirect_to new_customer_post_path
+      render :new
     end
   end
 
