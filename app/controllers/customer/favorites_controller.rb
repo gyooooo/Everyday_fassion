27d07dda039b811cdeddbfcc4649525cbd8aa031
gyooooo <harapeko2925@gmail.com>
2022-09-18 14:39:56 +0000

    コメント削除

--- app/controllers/customer/favorites_controller.rb
+++ app/controllers/customer/favorites_controller.rb
@@ -4,13 +4,13 @@ class Customer::FavoritesController < ApplicationController
     post = Post.find(params[:post_id])
     favorite = current_customer.favorites.new(post_id: post.id)
     favorite.save
-    # redirect_to customer_post_path(post)
+    redirect_to customer_post_path(post)
   end
   
   def destroy
     post = Post.find(params[:post_id])
     favorite = current_customer.favorites.find_by(post_id: post.id)
     favorite.destroy
-    # redirect_to customer_post_path(post)
+    redirect_to customer_post_path(post)
   end
 end
