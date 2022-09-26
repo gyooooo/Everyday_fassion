3bb0a8ae5f14aab6b6214293e9dee9a694ba0f7d
gyooooo <harapeko2925@gmail.com>
2022-09-26 10:54:42 +0000

    フォロー遷移先

--- app/controllers/customer/relationships_controller.rb
+++ app/controllers/customer/relationships_controller.rb
@@ -2,25 +2,15 @@ class Customer::RelationshipsController < ApplicationController
   before_action :set_customer, only: [:create, :destroy]
   
   def create
-    following = current_customer.follow(@customer)
-    if following.save
-      flash[:success] = 'ユーザーをフォローしました'
-      redirect_to customer_customer_path(@customer)
-    else
-      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_to customer_customer_path(@customer)
-    end
+    current_customer.follow(@customer)
+    flash[:success] = 'ユーザーをフォローしました'
+    redirect_back(fallback_location: root_url)
   end
 
   def destroy
-    following = current_customer.unfollow(@customer)
-    if following.destroy
-      flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_to customer_customer_path(@customer)
-    else
-      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_to customer_customer_path(@customer)
-    end
+    current_customer.unfollow(@customer)
+    flash[:success] = 'ユーザーのフォローを解除しました'
+    redirect_back(fallback_location: root_url)
   end
   
   def followings
